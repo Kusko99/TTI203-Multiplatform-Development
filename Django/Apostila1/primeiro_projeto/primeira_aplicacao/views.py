7a41cfefe626746b2af55829abf473ec848e0f95
Kusko99 <fernandaofo120@gmail.com>
2023-10-09 16:52:38 -0300

    Apostila1

---
+++ Django/Apostila1/primeiro_projeto/primeira_aplicacao/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    variaveis = {
+        'minha_primeira_variavel' : "Hello, variáveis, depois de alterar o diretório!!"
+    }
+    return render(request, "primeira_aplicacao/index.html", context=variaveis)
